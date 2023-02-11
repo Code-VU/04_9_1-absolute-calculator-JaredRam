31b7b3ecf7a802ee2fcfbf481cdc249bbf85738e
Jared Ramsey <Jared.Ramsey@vu.com>
2023-02-11 15:02:09 -0600

    Absolute difference

--- oddAbsolute.py
+++ oddAbsolute.py
@@ -1,7 +1,15 @@
 def calculateAbsolute():
     
     # This first line is provided for you
-    in_num  = input("Enter a number: ")
+    in_num  = (int(input("Enter a number: ")))
+
+    if in_num <= 21:
+        difference = 21 - in_num
+
+    else:
+        difference = (in_num - 21) * 2
+    
+    print("Result:", difference)
     # end assignment
 
 ## If you want to test locally run > python payCalculator.py
